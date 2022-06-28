b86acf9a34caeaedef9be0f773682170df342150
Simón Pedro <simon.pedro.g@gmail.com>
2022-06-28 08:44:09 -0300

    add licence to plot file

--- scludam/plots.py
+++ scludam/plots.py
@@ -1,3 +1,23 @@
+# scludam, Star CLUster Detection And Membership estimation package
+# Copyright (C) 2022  Simón Pedro González
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Module for helper plotting functions.
+
+"""
+
 import os
 import sys
 from copy import deepcopy
