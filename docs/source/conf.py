ca82a9abca06df0d047d1e97e7bd38e3a291c60e
Simón Pedro <simon.pedro.g@gmail.com>
2022-08-02 23:34:33 -0300

    change version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2022, Simón Pedro González"
 author = "Simón Pedro González"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
